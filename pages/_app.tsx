d89fbc478175d269c395efb58e6fe5e301eedc5b
ViniVMA <ctt.viniciusvilela@gmail.com>
2022-03-02 20:47:04 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Header } from "../src/components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="min-h-screen">
+        <div className="gradient-bg-welcome">
+          <Header />
+
+          <div
+            className="max-w-7xl m-auto "
+            style={{ border: "1px solid lime" }}
+          >
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ src/components/HeroSection/index.tsx
@@ -0,0 +1,13 @@
+export const HeroSection = () => {
+  return (
+    <div className="hero min-h-screen">
+      <div className=" "></div>
+      <div className="text-center hero-content text-neutral-content">
+        <div className="max-w-md">
+          <div className="artboard phone-4 artboard-demo">812&times;375</div>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
