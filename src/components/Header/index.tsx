4782731cdb8f19d6b42c010ae8857d5d0aceab1a
ViniVMA <ctt.viniciusvilela@gmail.com>
2022-03-10 17:10:24 -0300

    feat(Header): fix header nav texts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,13 +3,9 @@ function Header() {
     <nav className="bg-white border-gray-200 h-32 flex flex-col justify-center px-2 sm:px-4 py-2.5 rounded fixed w-full dark:bg-black/30">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <a href="#" className="flex items-center">
-          <img
-            src="/docs/images/logo.svg"
-            className="mr-3 h-6 sm:h-10"
-            alt="Flowbite Logo"
-          />
+          <img src="" className="mr-3 h-6 sm:h-10" alt="" />
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            Vinicius Vilela
           </span>
         </a>
         <button
@@ -64,22 +60,7 @@ function Header() {
                 About
               </a>
             </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Pricing
-              </a>
-            </li>
+
             <li>
               <a
                 href="#"
